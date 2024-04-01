747c9ff6038234958b584c8b92733d6981150013
abhiraj6 <165656217+abhiraj6@users.noreply.github.com>
2024-04-01 15:45:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// console.log("hello abhi");
+// var name="abhiraj"
+// console.log(name)
+var a=5
+var b=6
+if(a>b){
+    console.log("a is largest")
+}
+else{
+    console.log("b is largest")
+}
